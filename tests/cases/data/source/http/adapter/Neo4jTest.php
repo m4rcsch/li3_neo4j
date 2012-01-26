dee744d04d90fbba74f93522fac9a3111528552b
dgAlien <pti.ms@gmx.net>
2012-01-26 19:51:11 +0100

    added more code and reorganized tests

---
+++ tests/cases/data/source/http/adapter/Neo4jTest.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * li3_neo4j: a lithium database Adapter for the neo4j Graph Database
+ * Lithium: the most rad php framework
+ *
+ * @copyright     Copyright 2012, M.Schwering (http://github.com/dgAlien)
+ * @license       http://opensource.org/licenses/bsd-license.php The BSD License
+ */
+
+namespace li3_neo4j\tests\cases\data\source\http\adapter;
+
+use lithium\data\model\Query;
+use lithium\data\Connections;
+use lithium\data\entity\Document;
+use li3_neo4j\data\source\http\adapter\Neo4j;
+
+class Neo4jTest extends \lithium\test\Unit {
+
+	public $db;
+
+	protected $_configs = array();
+
+	protected $_testConfig = array(
+		'database' => 'lithium-test',
+		'persistent' => false,
+		'scheme' => 'tcp',
+		'host' => 'localhost',
+		'login' => 'root',
+		'password' => '',
+		'port' => 80,
+		'timeout' => 2,
+		'socket' => 'lithium\tests\mocks\data\source\http\adapter\MockSocket'
+	);
+
+	protected $_model = 'lithium\tests\mocks\data\source\http\adapter\MockCouchPost';
+
+	public function setUp() {
+		$this->_configs = Connections::config();
+
+		Connections::reset();
+		$this->db = new Neo4j(array('socket' => false));
+
+		Connections::config(array(
+			'mock-neo4j-connection' => array('object' => &$this->db, 'adapter' => 'Neo4j')
+		));
+
+		$model = $this->_model;
+		$entity = new Document(compact('model'));
+		$this->query = new Query(compact('model', 'entity'));
+	}
+
+	public function tearDown() {
+		Connections::reset();
+		Connections::config($this->_configs);
+		unset($this->query);
+	}
+}
+
+?>
