4be42d83ad28ab6ee2bc56a8a46d602d309f91c1
dgAlien <pti.ms@gmx.net>
2012-02-06 19:51:49 +0100

    added MockNeo4jPost

---
+++ tests/mocks/data/source/http/adapter/MockNeo4jPost.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Lithium: the most rad php framework
+ *
+ * @copyright     Copyright 2012, Union of RAD (http://union-of-rad.org)
+ * @license       http://opensource.org/licenses/bsd-license.php The BSD License
+ */
+
+namespace li3_neo4j\tests\mocks\data\source\http\adapter;
+
+class MockNeo4jPost extends \lithium\data\Model {
+
+	protected $_meta = array(
+		'source' => 'posts',
+		'connection' => 'mock-neo4j-connection'
+	);
+
+	protected $_schema = array(
+		'id' => array('type' => 'integer', 'key' => 'primary'),
+		'author_id' => array('type' => 'integer'),
+		'title' => array('type' => 'string', 'length' => 255),
+		'body' => array('type' => 'text'),
+		'created' => array('type' => 'datetime'),
+		'updated' => array('type' => 'datetime')
+	);
+}
+
+?>
